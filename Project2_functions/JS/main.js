e327acb85ce5bf3ff47758dc3451cdcb02d55d34
MatthewHogg1 <157215831+MatthewHogg1@users.noreply.github.com>
2024-05-26 16:37:55 +0100

    Add files via upload

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,24 @@
+//window.alert method
+window.alert("Hello, world!");
+
+//document.write method
+document.write("For the last time, hello, world!");
+
+//creating two variables
+var Mum = "Teresa";
+var Dad = "David";
+
+//Function
+function My_First_Function () {
+    var str = "This is the button text!";
+    var sentence = "I am learning"; //creating the variable
+    sentence += " a lot from this book!"; //using the += operator to add to the variable
+    document.getElementById("Concatenate").innerHTML = sentence; //using document.getElementByID()    
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+//displaying the variable using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(33 * 3)
