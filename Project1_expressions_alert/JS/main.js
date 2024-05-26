b824e56116fcf294dc345255673dadf7ad51d015
MatthewHogg1 <157215831+MatthewHogg1@users.noreply.github.com>
2024-05-26 16:04:45 +0100

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+//window.alert method
+window.alert("Hello, world!");
+
+//document.write method
+document.write("For the last time, hello, world!");
+
+//creating two variable
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string.";
+
+//displaying the variable using the document.write method
+document.write(Sent1 + Sent2);
+
+//an expression
+document.write(33 * 3)
